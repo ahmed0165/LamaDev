6f59e6fdea024bffaef329bfbfff2c4117148895
ahmed0165 <am5522031@gmail.com>
2024-10-30 17:07:50 +0300

    first commit

---
+++ next-dashboard-ui-completed/tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "login-page": "url(./public/loginPage.png)",
+      },
+      colors: {
+        lamaSky: "#C3EBFA",
+        lamaSkyLight: "#EDF9FD",
+        lamaPurple: "#CFCEFF",
+        lamaPurpleLight: "#F1F0FF",
+        lamaYellow: "#FAE27C",
+        lamaYellowLight: "#FEFCE8",
+      },
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        "50%": "50%",
+        "16": "4rem",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
---
+++ youtube-mern-ecommerce-app/youtube-mern-ecommerce-app/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDE2EmTPlqKm6OXE0Avj8kw9P49VdjQIb4",
+  authDomain: "shop-4a0f2.firebaseapp.com",
+  projectId: "shop-4a0f2",
+  storageBucket: "shop-4a0f2.appspot.com",
+  messagingSenderId: "701396538217",
+  appId: "1:701396538217:web:48e36259122853ae8e23f8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
